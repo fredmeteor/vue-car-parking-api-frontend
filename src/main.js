d1ea0ba79095b8df077e9892c45c96e2946e1297
fredmeteor <fredpeter09899@gmail.com>
2023-09-19 11:07:35 +0300

    Project scaffolding and other files

--- src/main.js
+++ src/main.js
@@ -1,11 +1,3 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
