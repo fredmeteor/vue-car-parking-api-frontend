052a236b3aab4f5aef63d65895e0147ff77d7de6
fredmeteor <fredpeter09899@gmail.com>
2023-09-19 12:38:25 +0300

    Added validation Error component

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,11 @@ const router = createRouter({
       name: 'home',
       component: import('../views/HomeView.vue'),
     },
+    {
+      path: "/register",
+      name: "register",
+      component: () => import("@/views/Auth/RegisterView.vue"),
+},
   ],
 })
 
---
+++ src/stores/register.js
@@ -0,0 +1,37 @@
+import { reactive } from "vue";
+import { defineStore } from "pinia";
+
+export const useRegister = defineStore("register", () => {
+  
+  const form = reactive({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+  });
+
+  const errors = reactive({});
+
+  function resetForm() {
+    form.name = "";
+    form.email = "";
+    form.password = "";
+    form.password_confirmation = "";
+  }
+
+  async function handleSubmit() {
+  	 errors.value = {};
+    return window.axios.post("auth/register", form).then((response) => {
+      console.log(response.data);
+    }).catch((error) => {
+      if (error.response.status === 422) {
+        errors.value = error.response.data.errors;
+      }
+    }).finally(() => {
+      form.password = "";
+      form.password_confirmation = "";
+    });
+  }
+
+  return { form, errors, resetForm, handleSubmit };
+});
